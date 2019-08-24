5bc50fd096c977955b7ac10233976f7b61d9e9eb
Your Name <you@example.com>
2019-08-24 10:22:35 -0700

    f

--- src/commands/information/ping.cmd.ts
+++ src/commands/information/ping.cmd.ts
@@ -11,8 +11,9 @@ export const command = new Command("ping", "Gets the bot ping.", [], [], "[verbo
 			const now = process.hrtime.bigint();
 			const msg = await message.channel.send(lang.commands.ping.calculating);
 			const diff = Number(process.hrtime.bigint() - now);
-			const speed = [0, 2e+7, 5e+7, 1e+8, 5e+9, 1e+10].findIndex(x => diff < x)
+			const speed = [0, 2e+7, 7e+7, 2e+8, 5e+9, 1e+10].findIndex(x => diff < x)
 			const speedstr = lang.commands.ping.speeds[5 - Math.max(0, speed)]
-			await msg.edit(client.utils.format(lang.commands.ping.pong, pms(diff / 1_000_000, { formatSubMilliseconds: true }), speedstr))
+			client.log(diff)
+			await msg.edit(client.utils.format(lang.commands.ping.pong, pms(diff / 1000000, { formatSubMilliseconds: true }), speedstr))
 		}
 	});
