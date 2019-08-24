41a573ca2e674eb0436c7e5bace3ff6684ddcfcc
Your Name <you@example.com>
2019-08-24 09:39:44 -0700

    fresh

--- src/commands/information/ping.cmd.ts
+++ src/commands/information/ping.cmd.ts
@@ -3,5 +3,10 @@ import { permissions } from "../../modules/permissions";
 export const command = new Command("ping", "Gets the bot ping.", [], [], "[verbose:bool]", permissions.everyone)
 	.setExec(async (client, message, args, lang) => {
 		if (!message.author) return;
-		const verbose = Boolean(args[0])
+		const verbose = Boolean(args[0]);
+		if (verbose) {
+
+		} else {
+			await message.channel.send(lang.commands.ping.calculating)
+		}
 	});
