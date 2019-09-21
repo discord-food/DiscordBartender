7f762f1ed804db5123c6fe73c432405cb6c0262c
Your Name <you@example.com>
2019-09-20 17:45:05 -0700

    fix

--- src/commands/ai/markov.cmd.ts
+++ src/commands/ai/markov.cmd.ts
@@ -2,9 +2,11 @@ import { Command } from "../../structures/command.struct";
 import { permissions } from "../../modules/permissions";
 import { GuildMember, EmbedField } from "discord.js";
 import { join } from "path"
-import MG from "markov-generator";
+import MS from "markov-strings";
 export const command = new Command("markov", "Markov Chain test.", [], [], [], permissions.everyone)
 	.setExec(async (client, message, args, lang) => {
-		const markov = new MG({input: (await client.models.Messages.findAll()).map(x => x.content), minLength: 20});
-		return message.channel.send(markov.makeChain());
+		const markov = new MS((await client.models.Messages.findAll()).map(x => x.content), { stateSize: 1 });
+		markov.buildCorpus();
+		const generated = markov.generate();
+		await message.channel.send(`${generated.string}\n------------------------\n**Tries**: ${generated.tries}\n**Score**: ${generated.score}`)
 	});
