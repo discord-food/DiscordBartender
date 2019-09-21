30dfb5a21ebfe9f83ce3f50fe5a041eeedfeacc7
Your Name <you@example.com>
2019-09-21 08:36:35 -0700

    adjustments

--- src/commands/ai/markov.cmd.ts
+++ src/commands/ai/markov.cmd.ts
@@ -3,11 +3,11 @@ import { permissions } from "../../modules/permissions";
 import { GuildMember, EmbedField } from "discord.js";
 import { join } from "path"
 import Markov from "js-markov";
-export const command = new Command("markov", "Markov Chain test.", [], ["mkv"], [{ name: "characters", type: Number }], permissions.everyone)
+export const command = new Command("markov", "Markov Chain test.", [], ["mkv"], [{ name: "characters", type: Number, default: 1000 }], permissions.everyone)
 	.setExec(async (client, message, args, lang) => {
 		const markov = new Markov();
 		markov.addStates((await client.models.Messages.findAll()).map(x => x.content));
-		markov.train();
+		markov.train(5);
 		try {
 			const generated = markov.generateRandom(args.characters); 
 			await message.channel.send(generated);
