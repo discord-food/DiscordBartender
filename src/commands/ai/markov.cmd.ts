42aca3f00df52e930f81d9bf368010b048770f4e
Your Name <you@example.com>
2019-09-21 08:43:42 -0700

    foop

--- src/commands/ai/markov.cmd.ts
+++ src/commands/ai/markov.cmd.ts
@@ -6,12 +6,13 @@ import Markov from "js-markov";
 export const command = new Command("markov", "Markov Chain test.", [], ["mkv"], [{ name: "characters", type: Number, default: 1000 }], permissions.everyone)
 	.setExec(async (client, message, args, lang) => {
 		const markov = new Markov();
-		markov.addStates((await client.models.Messages.findAll()).map(x => x.content));
+		const all = (await client.models.Messages.findAll()).map(x => x.content);
+		markov.addStates([...all, ...all, ...all]);
 		markov.train(5);
 		try {
 			const generated = markov.generateRandom(args.characters); 
 			await message.channel.send(generated);
 		} catch {
-			return message.channel.send(`Not enough training data.`);
-			}
+			return message.channel.send(`[no] Not enough training data.`);
+		}
 	});
