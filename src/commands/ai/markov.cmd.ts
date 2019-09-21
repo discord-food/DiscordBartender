5343599723893da9f03fc9bc28aa0b8356a07814
Your Name <you@example.com>
2019-09-20 20:57:33 -0700

    fff

--- src/commands/ai/markov.cmd.ts
+++ src/commands/ai/markov.cmd.ts
@@ -3,13 +3,13 @@ import { permissions } from "../../modules/permissions";
 import { GuildMember, EmbedField } from "discord.js";
 import { join } from "path"
 import Markov from "js-markov";
-export const command = new Command("markov", "Markov Chain test.", [], [], [], permissions.everyone)
+export const command = new Command("markov", "Markov Chain test.", [], ["mkv"], [{ name: "characters", type: Number }], permissions.everyone)
 	.setExec(async (client, message, args, lang) => {
 		const markov = new Markov();
 		markov.addStates((await client.models.Messages.findAll()).map(x => x.content));
 		markov.train();
 		try {
-			const generated = markov.generateRandom(); 
+			const generated = markov.generateRandom(args.characters); 
 			await message.channel.send(generated);
 		} catch {
 			return message.channel.send(`Not enough training data.`);
