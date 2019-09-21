0079cf9ffa321140951ef9f44bdbbe713efa74a3
Your Name <you@example.com>
2019-09-20 17:49:03 -0700

    better

--- src/commands/ai/markov.cmd.ts
+++ src/commands/ai/markov.cmd.ts
@@ -5,7 +5,7 @@ import { join } from "path"
 import MS from "markov-strings";
 export const command = new Command("markov", "Markov Chain test.", [], [], [], permissions.everyone)
 	.setExec(async (client, message, args, lang) => {
-		const markov = new MS((await client.models.Messages.findAll()).map(x => x.content), { stateSize: 1 });
+		const markov = new MS((await client.models.Messages.findAll()).map(x => x.content).filter(x => x.split(" ").length > 2), { stateSize: 3 });
 		markov.buildCorpus();
 		const generated = markov.generate();
 		await message.channel.send(`${generated.string}\n------------------------\n**Tries**: ${generated.tries}\n**Score**: ${generated.score}`)
