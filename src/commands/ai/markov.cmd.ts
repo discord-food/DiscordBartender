5042baf59935736e49718b31aecb3f34bab629a8
Your Name <you@example.com>
2019-09-20 18:00:16 -0700

    fix

--- src/commands/ai/markov.cmd.ts
+++ src/commands/ai/markov.cmd.ts
@@ -5,8 +5,12 @@ import { join } from "path"
 import MS from "markov-strings";
 export const command = new Command("markov", "Markov Chain test.", [], [], [], permissions.everyone)
 	.setExec(async (client, message, args, lang) => {
-		const markov = new MS((await client.models.Messages.findAll()).map(x => x.content).filter(x => x.split(" ").length > 2), { stateSize: 3 });
+		const markov = new MS((await client.models.Messages.findAll()).map(x => x.content).filter(x => x.split(" ").length > 2), { stateSize: 2 });
 		markov.buildCorpus();
-		const generated = markov.generate();
-		await message.channel.send(`${generated.string}\n------------------------\n**Tries**: ${generated.tries}\n**Score**: ${generated.score}`)
+		try {
+			const generated = markov.generate(); 
+			await message.channel.send(`${generated.string}\n------------------------\n**Tries**: ${generated.tries} | **Score**: ${generated.score}`);
+		} catch {
+			return message.channel.send(`Not enough training data.`);
+			}
 	});
