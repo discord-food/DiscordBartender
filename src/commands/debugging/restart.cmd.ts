44dd00d19bcd1d8d428b15c230079a1888310304
TNThacker2015 <you@example.com>
2019-10-03 17:58:02 -0700

    Ate some mustard

--- src/commands/debugging/restart.cmd.ts
+++ src/commands/debugging/restart.cmd.ts
@@ -2,8 +2,8 @@ import { execSync } from "child_process";
 import { EmbedField, GuildMember, Util } from "discord.js";
 import { transpile } from "typescript";
 import { permissions } from "../../modules/permissions";
-import { Command } from "@db-struct/command";
-export const command = new Command("restart", "Restart the bot.", [], [], [], permissions.admin)
+import { Command } from "@db-struct/command.struct";
+export const command = new Command("restart", "Restarts the bot.", [], [], [], permissions.admin)
 	.setExec(async(client, message, args, lang) => {
 		await message.channel.send("Restarting...");
 		client.exec("yarn run ez");
