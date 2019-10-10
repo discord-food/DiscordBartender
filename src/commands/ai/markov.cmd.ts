b41cfaa0c97fd784a3befdaf0cbc30691fc99020
TNThacker2015 <you@example.com>
2019-10-09 18:47:23 -0700

    Killed some mystic dough

--- src/commands/ai/markov.cmd.ts
+++ src/commands/ai/markov.cmd.ts
@@ -4,12 +4,12 @@ import { permissions } from "../../modules/permissions";
 import { Command } from "@db-struct/command.struct";
 import { Markov } from "@db-struct/markov.struct";
 export const command = new Command("markov", "Markov Chain test.", [], ["mkv"], [
-	{ name: "start", type: String, default: "" },
+	{ name: "start", type: String },
 	{ name: "mode", type: Command.OR("word", "char"), default: "word" },
 	{ name: "order", type: Number, default: 2 },
 ], permissions.everyone)
 	.setExec(async(client, message, args, lang) => {
 		const all = (await client.models.Messages.findAll()).map(x => x.content).filter(x => x.split(/\s+/).length > 2);
 		const markov = new Markov(all, args.mode, args.order);
-		await message.channel.send(markov.generate(args.start.split(/\s+/)));
+		await message.channel.send(markov.generate(args.start ? args.start.split(/\s+/) : undefined));
 	});
