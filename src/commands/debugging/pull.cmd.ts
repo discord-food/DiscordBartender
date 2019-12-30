4dc815e1e268d7e8832c0025ac131d98f49829e0
SYOfficial <stephen6678@hotmail.com>
2019-12-30 11:10:06 -0800

    Made some mustard

--- src/commands/debugging/pull.cmd.ts
+++ src/commands/debugging/pull.cmd.ts
@@ -4,4 +4,4 @@ import { transpile } from "typescript";
 import { permissions } from "../../modules/permissions";
 import { Command } from "@db-struct/command.struct";
 export const command = new Command("pull", "Pull the latest code.", [], [], [] as const, permissions.admin)
-	.setExec(async(client, message, args, lang) => message.channel.send(client.exec("git pull"), { code: "bash" }));
+	.setExec(async(client, message, args, lang) => message.channel.send(client.exec("git add -A && git commit -m Commit && git pull"), { code: "bash" }));
