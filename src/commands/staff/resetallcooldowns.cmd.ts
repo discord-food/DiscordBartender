593f1e780cf900ef69ae9f3c0d11dedc370a148a
SYOfficial <stephen6678@hotmail.com>
2019-12-30 11:16:55 -0800

    Composted gagi

--- src/commands/staff/resetallcooldowns.cmd.ts
+++ src/commands/staff/resetallcooldowns.cmd.ts
@@ -3,5 +3,10 @@ import { permissions } from "../../modules/permissions";
 import { Command } from "@db-struct/command.struct";
 export const command = new Command("resetallcooldowns", "Reset all users' cooldowns.", [], ["rac"], [] as const, permissions.moderator)
 	.setExec(async(client, message, args, lang) => {
-
+		await client.models.Userinfo.createQueryBuilder()
+			.update()
+			.set({ cooldowns: {} })
+			.where("1 = 1")
+			.execute();
+		await message.channel.send(`All cooldowns have been reset.`);
 	});
--- src/commands/staff/resetcooldowns.cmd.ts
+++ src/commands/staff/resetcooldowns.cmd.ts
@@ -1,10 +1,13 @@
 import { EmbedField, GuildMember } from "discord.js";
 import { permissions } from "../../modules/permissions";
 import { Command } from "@db-struct/command.struct";
-export const command = new Command("resetcooldowns", "Reset a user's cooldowns.", ["money", "balance"], ["bal", "acc"], [{
+export const command = new Command("resetcooldowns", "Reset a user's cooldowns.", [], ["rc"], [{
 	name: "user",
 	type: Command.USER({ self: true }),
 }] as const, permissions.everyone)
 	.setExec(async(client, message, args, lang) => {
-
+		const account = await client.getAccount(args.user.id);
+		account.cooldowns = {};
+		account.save();
+		await message.channel.send(`All cooldowns for user \`${args.user.tag}\` have been reset.`);
 	});
