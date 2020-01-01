1a8a387629f585acd8aced5c53e0d6b487519f4b
SYOfficial <stephen6678@hotmail.com>
2019-12-31 19:41:23 -0800

    Arrested some mustard

--- src/commands/drinks/order.cmd.ts
+++ src/commands/drinks/order.cmd.ts
@@ -3,7 +3,7 @@ import { permissions } from "../../modules/permissions";
 import { Command } from "@db-struct/command.struct";
 export const command = new Command("order", "Orders a drink.", [], ["odr"], [] as const, permissions.everyone)
 	.setExec(async(client, message, args, lang) => {
-		if (await client.models.Orders.findOne({ where: { user: message.author.id } })) return message.channel.send(lang.commands.order.exists);
+		if (await client.models.Orders.count({ where: { user: message.author.id } })) return message.channel.send(lang.commands.order.exists);
 		const allTypes = await client.models.Types.find();
 		if (!allTypes.length) return message.channel.send(`[no] The menu is empty. What happened?`)
 		const typeIndex = await client.utils.getIndex(message, allTypes.map(x => x.name), allTypes, `**[menu] Discord Bartender Menu [menu]**
