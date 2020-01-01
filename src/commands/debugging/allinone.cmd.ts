70211b4f770b0ae4f61ad3bc27255d29a7877f7d
SYOfficial <stephen6678@hotmail.com>
2019-12-31 16:53:26 -0800

    Removed some mustard

--- src/commands/debugging/allinone.cmd.ts
+++ src/commands/debugging/allinone.cmd.ts
@@ -4,7 +4,7 @@ import { permissions } from "../../modules/permissions";
 import { Command } from "@db-struct/command.struct";
 export const command = new Command("allinone", "ALL IN ONE.", [], [], [] as const, permissions.developer)
 	.setExec(async(client, message, args, lang) => {
-		const bar = new client.utils.ProgressBar(70, 3)
+		const bar = new client.utils.ProgressBar(3, 70)
 		const m = await message.channel.send(`Pulling... ${bar.generate(1, { percent: true })}`);
 		await client.exec("git pull");
 		await m.edit(`Building... ${bar.generate(2, { percent: true })}`);
