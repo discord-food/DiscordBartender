16ac0f0947a66774662c94607a31fed20115c768
SYOfficial <stephen6678@hotmail.com>
2019-12-31 19:19:49 -0800

    Asserted that 1 == 1

--- src/commands/drinks/list.cmd.ts
+++ src/commands/drinks/list.cmd.ts
@@ -3,8 +3,8 @@ import { permissions } from "../../modules/permissions";
 import { Command } from "@db-struct/command.struct";
 export const command = new Command("list", "List all available orders.", [], ["lst"], [] as const, permissions.staff)
 	.setExec(async(client, message, args, lang) => {
-		const orders = (await client.models.Orders.find({ relations: ["type"] })).filter(x => x.available);
+		const orders = (await client.models.Orders.find({ relations: ["type"] })).filter(x => x.available).sort((x, y) => (x.status ?? 0) - (y.status ?? 0));
 		if (!orders) return message.channel.send(lang.commands.list.noOrders);
 		await message.channel.send(`>>> **__Current Unclaimed Orders__**:
-${orders.map(x => `\`*${x.id}\`: **${x.statusString}${x.status === client.sql.Status.PREPARING ? client.users.get(x.metadata.claimer ?? "")?.tag ?? "Unknown User#0000" : ""}** - ${x.description ?? x.type.name}`).join("\n")}`)
+${orders.map(x => `\`${x.id}\`: **${x.statusString}${x.status === client.sql.Status.PREPARING ? client.users.get(x.metadata.claimer ?? "")?.tag ?? "Unknown User#0000" : ""}** - ${x.description ?? x.type.name}`).join("\n")}`)
 	});
