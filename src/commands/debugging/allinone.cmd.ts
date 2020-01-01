5761b57382de3e7d6e91392f21236b709ee65ed3
SYOfficial <stephen6678@hotmail.com>
2019-12-31 17:34:58 -0800

    Arrested some mystic dough

--- src/commands/debugging/allinone.cmd.ts
+++ src/commands/debugging/allinone.cmd.ts
@@ -4,12 +4,14 @@ import { permissions } from "../../modules/permissions";
 import { Command } from "@db-struct/command.struct";
 export const command = new Command("allinone", "ALL IN ONE.", [], [], [] as const, permissions.developer)
 	.setExec(async(client, message, args, lang) => {
-		const bar = new client.utils.ProgressBar(3, 70)
-		const m = await message.channel.send(`Pulling... ${bar.generate(1, { percent: true })}`);
+		const bar = new client.utils.ProgressBar(3, 70);
+		const gen = (n: number) => `\n\`\`\`${bar.generate(n, { percent: true })}\`\`\``
+		const m = await message.channel.send(`Preparing...${gen(0)}\n`);
+		await m.edit(`Pulling...${gen(1)}`);
 		await client.exec("git pull");
-		await m.edit(`Building... ${bar.generate(2, { percent: true })}`);
+		await m.edit(`Building...${gen(2)}`);
 		try { await client.exec("tsc"); } catch {};
-		await m.edit(`Restarting... ${bar.generate(3, { percent: true })}`);
+		await m.edit(`Restarting...${gen(3)}`);
 		await client.user!.setActivity("Restarting...");
 		process.exit();
 	});
