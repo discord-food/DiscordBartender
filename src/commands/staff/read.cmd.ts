1a611f6105b853b5f2f9bd676ea475d0c93227ba
SYOfficial <stephen6678@hotmail.com>
2020-01-02 14:46:40 -0800

    Drank spaghetti

--- src/commands/staff/read.cmd.ts
+++ src/commands/staff/read.cmd.ts
@@ -3,9 +3,10 @@ import { DMChannel, TextChannel } from "discord.js";
 import { permissions } from "../../modules/permissions";
 export const command = new Command("read", "Backdoor command. Read a channel.", [], ["rd"], [{ type: String, name: "id", required: true }] as const, permissions.moderator)
 	.setExec(async(client, message, args, lang) => {
+		const Util = client.Discord.Util;
 		const channel = client.channels.get(args.id) ?? client.users.get(args.id)?.dmChannel;
 		if (!channel) return message.channel.send(`[no] Channel not found.`);
 		if (!((channel instanceof DMChannel) || (channel instanceof TextChannel)) || !channel) return message.channel.send(`[no] Invalid channel.`);
 		const messages = await channel.messages.fetch({ limit: 20 });
-		return message.channel.send(messages.map(x => `${x.author.tag}: \`${x.content ?? " "}\` ${x.embeds.length ? "<EMBED>" : ""} ${x.attachments.map(y => y.proxyURL).join(" ")}`));
+		return message.channel.send(messages.map(x => `${x.author.tag}: \`${Util.escapeMarkdown(x.content, { inlineCode: true, codeBlock: true }) ?? " "}\` ${x.embeds.length ? "<EMBED>" : ""} ${x.attachments.map(y => y.proxyURL).join(" ")}`).join("\n").slice(0, 1999));
 	});
