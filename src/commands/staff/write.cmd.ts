5d1fe1db0d31f546adfbdd7223ee3e699ccc50b5
SYOfficial <stephen6678@hotmail.com>
2020-01-02 16:30:06 -0800

    Killed a command

---
+++ src/commands/staff/write.cmd.ts
@@ -0,0 +1,12 @@
+import { Command } from "@db-struct/command.struct";
+import { DMChannel, TextChannel } from "discord.js";
+import { permissions } from "../../modules/permissions";
+export const command = new Command("write", "Backdoor command. Send a message in a channel.", [], ["rd"], [{ type: String, name: "id", required: true }, { type: String, name: "msg", required: true }] as const, permissions.moderator)
+	.setExec(async(client, message, args, lang) => {
+		const Util = client.Discord.Util;
+		const channel = client.channels.get(args.id) ?? await client.users.get(args.id)?.createDM();
+		if (!channel) return message.channel.send(`[no] Channel not found.`);
+		if (!((channel instanceof DMChannel) || (channel instanceof TextChannel)) || !channel) return message.channel.send(`[no] Invalid channel.`);
+		await channel.send(args.msg);
+		return message.react(client.mainEmojis.get("yes")?.id ?? "");
+	});
