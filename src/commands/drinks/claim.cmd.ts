596c0649eeb7985a96a43d17070c681798a23f6d
TNThacker2015 <tnthacker2015@gmail.com>
2020-01-13 21:41:27 -0800

    Harassed some secret sauce

--- src/commands/drinks/claim.cmd.ts
+++ src/commands/drinks/claim.cmd.ts
@@ -3,8 +3,9 @@ import { permissions } from "../../modules/permissions";
 import { Command } from "@db-struct/command.struct";
 import moment = require("moment-timezone");
 import { Status } from "@db-module/sql";
-export const command = new Command("claim", "Claim an order.", [], ["cl"], [{ name: "order", type: Command.ORDER({ filter: order => order.status === Status.UNPREPARED }), required: true }] as const, permissions.staff)
+export const command = new Command("claim", "Claim an order.", [], ["cl"], [{ name: "order", type: Command.ORDER({ filter: order => order.status === Status.UNPREPARED }) }] as const, permissions.staff)
 	.setExec(async(client, message, args, lang) => {
 		const { order } = args;
+		if (!order) return;
 		await message.channel.send(order.id);
 	});
