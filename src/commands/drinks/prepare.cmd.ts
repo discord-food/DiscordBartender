19fcccadb1db239703607c255189c243f2dda201
TNThacker2015 <tnthacker2015@gmail.com>
2020-02-02 21:21:26 -0800

    Fixed a speeding gagi

--- src/commands/drinks/prepare.cmd.ts
+++ src/commands/drinks/prepare.cmd.ts
@@ -9,7 +9,7 @@ export const command = new Command("prepare", "Prepare your current claimed orde
 		const order = await client.getClaimedOrder(message.author.id);
 		if (!order) return message.channel.send(lang.errors.noClaimedOrder);
 		if (order.status !== Status.PREPARING) return message.channel.send(lang.errors.notPreparing);
-		const checkPrepared = async() => order.type.recipe.every(x => order.prepared.includes(x.id)) && (order.status = Status.PENDING_DELIVERY, message.channel.send("The order is fully prepared! It is now pending delivery."));
+		const checkPrepared = async() => order.type.recipe.every(x => order.prepared.includes(x.id)) && (order.status = Status.PENDING_DELIVERY, await order.save(), message.channel.send("The order is fully prepared! It is now pending delivery."));
 		if (await checkPrepared()) return;
 		const embed = new client.Embed(false)
 			.setTitle(`Ingredient Checklist for \`${order.id}\``)
