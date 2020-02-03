793747ba7c35aac21ed4f536a142cc951cb1a180
TNThacker2015 <tnthacker2015@gmail.com>
2020-02-03 07:37:05 -0800

    Composted a speeding gagi

--- src/commands/drinks/list.cmd.ts
+++ src/commands/drinks/list.cmd.ts
@@ -2,7 +2,7 @@ import { EmbedField, GuildMember } from "discord.js";
 import { permissions } from "../../modules/permissions";
 import { Command } from "@db-struct/command.struct";
 import moment = require("moment-timezone");
-export const command = new Command("list", "List all available orders.", [], ["lst"], [] as const, permissions.staff)
+export const command = new Command("list", "List all available orders.", [], ["lst", "li", "ls"], [] as const, permissions.staff)
 	.setExec(async(client, message, args, lang) => {
 		const orders = (await client.models.Orders.find({ order: { createdAt: "ASC" } })).filter(x => x.available).sort((x, y) => (x.status ?? 0) - (y.status ?? 0));
 		if (!orders) return message.channel.send(lang.commands.list.noOrders);
