121a75f67851ae470eb7960282635ed0de5ff7ef
TNThacker2015 <tnthacker2015@gmail.com>
2020-02-03 07:37:12 -0800

    Composted some mustard

--- src/commands/drinks/brew.cmd.ts
+++ src/commands/drinks/brew.cmd.ts
@@ -11,7 +11,7 @@ export const command = new Command("brew", "Brew your current claimed order.", [
 		if (!image) return;
 		if (!image.match(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/)) return message.channel.send(lang.errors.url);
 		order.metadata.image = image;
-		order.status = Status.PENDING_DELIVERY;
+		order.status = Status.FERMENTING;
 		await order.save();
 		await message.channel.send(lang.commands.brew.success);
 	});
