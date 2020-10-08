ee318df58629b4fb2bacc38ab1d3cac127aa413b
TNThacker2015 <tnthacker2015@gmail.com>
2020-10-08 10:36:14 -0700

    blacklist

--- src/commands/staff/unblacklist.cmd.ts
+++ src/commands/staff/unblacklist.cmd.ts
@@ -1,7 +1,7 @@
 import { Command } from "@db-struct/command.struct";
 import { DMChannel, TextChannel } from "discord.js";
 import { permissions } from "../../modules/permissions";
-export const command = new Command("unblacklist", "Unblacklist an id.", [], ["bl"], [{ type: String, name: "id", required: true }] as const, permissions.botModerator)
+export const command = new Command("unblacklist", "Unblacklist an id.", [], ["unbl"], [{ type: String, name: "id", required: true }] as const, permissions.botModerator)
 	.setExec(async(client, message, args, lang) => {
 		await client.models.Blacklist.delete({ id: args.id });
 		await message.channel.send("The id has been successfully unblacklisted.");
